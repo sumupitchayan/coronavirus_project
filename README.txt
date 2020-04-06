ded7ec3302023ee3bb16573b3e9d886e8dfa1224
Rudra Srivastava <rudrasrivastava@Rudras-MacBook-Pro.local>
2020-04-07 00:50:46 +0530

    added government effectiveness

---
+++ README.txt
@@ -0,0 +1,7 @@
+README
+
+Sources
+
+1) Johns Hopkins Data
+2) Worldometer (Scraping)
+3) https://ourworldindata.org/covid-testing
--- collection.py
+++ collection.py
@@ -4,6 +4,8 @@ import numpy as np
 import requests
 from bs4 import BeautifulSoup
 import json
+import plotly.express as px
+import math
 
 df = pd.read_csv("time_series_covid19_confirmed_global.csv")
 
@@ -35,10 +37,28 @@ for row in items[1:]:
     if age != "N.A.":
         infections[country]['median_age'] = int(age.strip())
 
-keys = list(infections.keys())  # data cleaning - remove countries with no population/median age estimate
+df = pd.read_csv("gov_effect.csv")
+
+for row in df.values:
+    if row[0] not in infections or row[1] != row[1]:
+        continue
+    infections[row[0]]['government_effectiveness'] = float(row[1])
+
+# print(len(infections)) # 181 here
+keys = list(infections.keys())  # data cleaning - remove countries with no population/median age/gov_effect estimate
 for i in range(len(keys)):
-    if len(list(infections[keys[i]].keys())) != 4:
+    if len(list(infections[keys[i]].keys())) != 5:
         del infections[keys[i]]
 
+# print(len(infections)) # 147 here
 with open('infections.json', 'w') as outfile:
     json.dump(infections, outfile, indent=4, sort_keys=True)
+
+
+
+# ---------------------GRAPHING---------------------------
+
+data = [list(infections.keys()), [infections[k]["total_infections"] for k in infections], [infections[k]["government_effectiveness"] for k in infections]]
+fig = px.scatter(data, x = data[1], y = data[2], text = data[0], log_x = True)
+fig.update_traces(textposition='top center')
+fig.show()
