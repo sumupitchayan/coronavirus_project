aad85b569d0a599d0c23952a04038cc79c4e6675
Rudra Srivastava <rudrasrivastava@Rudras-MacBook-Pro.local>
2020-04-07 14:26:08 +0530

    added testing

--- README.txt
+++ README.txt
@@ -3,6 +3,29 @@ README
 Sources
 
 1) Johns Hopkins Data
-2) Worldometer (Scraping)
+2) Worldometer (Scraping for population and testing)
 3) https://ourworldindata.org/covid-testing
 4) World Bank Data
+
+
+Variables:
+
+1) country
+3) total population
+2) total infections per capita
+3) max infections in single day per capita
+4) government effectiveness measure (between -2.5 and 2.5)
+5) median age
+6) total tests per capita
+
+Instructions:
+
+Script will automatically remove data that is not complete. In order to add data you must change the required values to reflect
+the new variable (right now it is 6 you should change it to however many total variables present). Make sure you compare your scraping/new dataset
+country names to country names in the database and add that to aliases (US = USA = United States). A data value should not be deleted
+just because it has a variation of a name. Uncomment out the print statements in cleaning portion to see which values were deleted and which ones you should
+double check. To put figures out there, original db had 181 countries however after cleaning only 110 are remaining. 
+
+Data: 
+
+data is stored in infections.json.
--- collection.py
+++ collection.py
@@ -18,6 +18,18 @@ for c in range(len(countries)):
     total = np.sum(matrix, axis = 0)
     infections[countries[c]] = {"total_infections": np.sum(total), "max_infections": np.max(total)}
 
+aliases = {  # value is value in database/ key is value that may be from other sources
+    "USA": "US", 
+    "S. Korea": "Korea, South", 
+    "UK": "United Kingdom",
+    "Burma": "Myanmar", 
+    "United States": "US", 
+    "Russian Federation": "Russia",
+    "Lao PDR": "Laos",
+    "UAE": "United Arab Emirates",
+    "Brunei Darussalam": "Brunei"
+    }
+
 res = requests.get("https://www.worldometers.info/world-population/population-by-country/")  #scraping population
 soup = BeautifulSoup(res.text, 'html.parser')
 
@@ -29,6 +41,8 @@ for row in items[1:]:
     pop = information[2]
     age = information[9].text.strip()
     info = ''.join(pop.text.strip().split(","))
+    if country in aliases:
+        country = aliases[country]
     if not info or country not in infections:
         continue
     infections[country]['population'] = int(info)
@@ -37,20 +51,51 @@ for row in items[1:]:
     if age != "N.A.":
         infections[country]['median_age'] = int(age.strip())
 
+
+res = requests.get("https://www.worldometers.info/coronavirus/")  #scraping testing
+soup = BeautifulSoup(res.text, 'html.parser')
+
+items = soup.find("table").find_all("tr")
+
+
+for row in items[1:]:
+    information = row.find_all('td')
+    country = information[0].text.strip()
+    test = information[10]
+    info = ''.join(test.text.strip().split(","))
+
+    if country in aliases:
+        country = aliases[country]
+    if not info or country not in infections:
+        continue
+    
+    infections[country]['total_tests'] = int(info)/infections[country]['population']
+
+
 df = pd.read_csv("gov_effect.csv")
 
 for row in df.values:
-    if row[0] not in infections or row[1] != row[1]:
+    country = row[0]
+
+    if country in aliases:
+        country = aliases[country]
+
+    if country not in infections or row[1] != row[1]:
         continue
-    infections[row[0]]['government_effectiveness'] = float(row[1])
+
+    infections[country]['government_effectiveness'] = float(row[1])
 
 # print(len(infections)) # 181 here
-keys = list(infections.keys())  # data cleaning - remove countries with no population/median age/gov_effect estimate
+
+removed_countries = []
+keys = list(infections.keys())  # data cleaning - remove countries with no population/median age/gov_effect/testing estimate
 for i in range(len(keys)):
-    if len(list(infections[keys[i]].keys())) != 5:
+    if len(list(infections[keys[i]].keys())) != 6:
+        removed_countries.append(keys[i])
+        #print(keys[i], infections[keys[i]]) #UNCOMMENT THIS LINE IF YOU WANT TO SEE WHICH VALUES ARE MISSING FROM WHAT COUNTRIES
         del infections[keys[i]]
 
-# print(len(infections)) # 147 here
+# print(len(infections)) # 110 here - lot of countries have missing testing data
 with open('infections.json', 'w') as outfile:
     json.dump(infections, outfile, indent=4, sort_keys=True)
 
@@ -58,7 +103,7 @@ with open('infections.json', 'w') as outfile:
 
 # ---------------------GRAPHING---------------------------
 
-data = [list(infections.keys()), [infections[k]["total_infections"] for k in infections], [infections[k]["government_effectiveness"] for k in infections]]
+data = [list(infections.keys()), [infections[k]["total_infections"] for k in infections], [infections[k]["total_tests"] for k in infections]]
 fig = px.scatter(data, x = data[1], y = data[2], text = data[0], log_x = True)
 fig.update_traces(textposition='top center')
 fig.show()
