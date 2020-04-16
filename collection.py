44fd076c331e34f7bf1796c1cb13830909be1dbc
Snigdha Sinha <Snigdha@Snigdhas-Macbook.local>
2020-04-16 20:48:45 +0800

    updated dataset

--- collection.py
+++ collection.py
@@ -9,7 +9,7 @@ import math
 import csv
 
 print("Running..")
-df = pd.read_csv("data/time_series_covid19_confirmed_global.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
 
 
 countries = df["Country/Region"].unique()
--- proof_to_change.py
+++ proof_to_change.py
@@ -6,6 +6,8 @@ from bs4 import BeautifulSoup
 import json
 import plotly.express as px
 import math
+import plotly.offline as offline
+
 
 print("Running..")
 df = pd.read_csv("data/time_series_covid19_confirmed_global.csv")
@@ -83,21 +85,44 @@ data = data[1:]
 
 data = pd.DataFrame(data, columns=["Date","China","Other Countries"])
 data.drop(0)
-print(data.dtypes)
 # data['China'][0] = np.log(data['China'][0])
 # print(data['China'][0])
 data['China'] = data['China'].astype(str).astype(int)
 data['Other Countries'] = data['Other Countries'].astype(str).astype(int)
 data['log_China'] = np.log(data['China'])
 data['log_Other_Countries'] = np.log(data['Other Countries'])
-print(data)
+
+
+
+df_melt = pd.melt(data,id_vars='Date', value_vars=["China", "Other Countries"])
+
+fig = px.line(df_melt, x='Date' , y='value' , color='variable')
+fig.update_layout(title="Infections Over Time",legend_title='<b> Country </b>',yaxis_title="log of infections per 1million",width=1000,
+    height=600)
+fig.add_shape(
+        # Line Vertical
+        dict(
+            type="line",
+            x0='3/13/20',
+            y0=-100000,
+            x1='3/13/20',
+            y1=2100000,
+            line=dict(
+                color="LightGreen",
+                width=3
+            )))
+    
+fig.show()
+
+
 
 
 
 df_melt = pd.melt(data,id_vars='Date', value_vars=["log_China", "log_Other_Countries"])
 
 fig = px.line(df_melt, x='Date' , y='value' , color='variable')
-fig.update_layout(legend_title='<b> Country Cases (log) </b>')
+fig.update_layout(title="Log of Infections Over Time",legend_title='<b> Country (log) </b>',yaxis_title="log of infections per 1million",width=1000,
+    height=600)
 fig.add_shape(
         # Line Vertical
         dict(
@@ -105,7 +130,7 @@ fig.add_shape(
             x0='3/13/20',
             y0=0,
             x1='3/13/20',
-            y1=15,
+            y1=16,
             line=dict(
                 color="LightGreen",
                 width=3
